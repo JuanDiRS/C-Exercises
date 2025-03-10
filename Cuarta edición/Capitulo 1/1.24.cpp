8ad599a924fee670c3f678c669ba6674ea8dbf85
Juan Diego Rubiano <juandirusaenz@gmail.com>
2025-03-10 14:17:00 -0500

    Create 1.24.cpp

---
+++ Cuarta edición/Capitulo 1/1.24.cpp
@@ -0,0 +1,23 @@
+//Realizaremos una tarea simple la cual sera mostrar numeros del 1 al 4 pero lo haremos de manera muy simople y de tres maneras diferentes segun las siguientes condiciones:
+
+//- Con una instruccion de salida y un operador de insercion de flujo.
+#include <iostream>
+int main()
+{
+    std::cout<<"1 2 3 4";
+}
+//- Con una sola instruccion de salida y cuatro operadores de insercion de flujo.
+#include <iostream>
+int main()
+{
+    std::cout<<"1 "<<"2 "<<"3 "<<"4";
+}
+//Con cuatro instrucciones de salida.
+#include <iostream>
+int main()
+{
+    std::cout<<"1 ";
+    std::cout<<"2 ";
+    std::cout<<"3 ";
+    std::cout<<"4";
+}
