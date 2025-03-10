5b034209cada2eab1ab78d8a9ac77d1577b308b6
Juan Diego Rubiano <juandirusaenz@gmail.com>
2025-03-10 12:16:38 -0500

    Create 1.6.cpp

---
+++ Cuarta edición/Capitulo 1/1.6.cpp
@@ -0,0 +1,19 @@
+//Calculo de tres enteros para este ejercicio haremos uso de instrucciones using.
+
+#include <iostream>
+using std::cout;
+using std::cin;
+
+int main()
+{
+int x;
+int y;
+int z;
+int res;
+cout<<"introduzca tres numeros utilizando enter entre cada numero: ";
+cin>>x>>y>>z;
+res = x*y*z;
+cout<<"El producto es "<<res;
+
+return 0;
+}
