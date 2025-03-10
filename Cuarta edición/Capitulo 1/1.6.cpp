027823fe0ef26ae019bafd79c5460492ad5cbe73
Juan Diego Rubiano <juandirusaenz@gmail.com>
2025-03-10 12:41:17 -0500

    Update 1.6.cpp

--- Cuarta edición/Capitulo 1/1.6.cpp
+++ Cuarta edición/Capitulo 1/1.6.cpp
@@ -1,19 +1,17 @@
 //Calculo de tres enteros para este ejercicio haremos uso de instrucciones using.
-
 #include <iostream>
 using std::cout;
 using std::cin;
 
 int main()
 {
-int x;
-int y;
-int z;
-int res;
-cout<<"introduzca tres números: ";
-cin>>x>>y>>z;
-res = x*y*z;
-cout<<"El producto es "<<res;
-
-return 0;
+  int x;
+  int y;
+  int z;
+  int res;
+  cout<<"introduzca tres números: ";
+  cin>>x>>y>>z;
+  res = x*y*z;
+  cout<<"El producto es "<<res;
+  return 0;
 }
