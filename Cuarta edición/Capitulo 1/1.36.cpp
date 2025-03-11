8abf5cefaaef7114bbbc7b924a4b5b1af569dc1f
Juan Diego Rubiano <juandirusaenz@gmail.com>
2025-03-10 21:37:41 -0500

    Create 1.36.cpp

---
+++ Cuarta edición/Capitulo 1/1.36.cpp
@@ -0,0 +1,17 @@
+//El programa recibira un numero y devolvera este numero pero con dos espacios entre cada uno de los digitos del numero.
+#include <iostream>
+using std::cout;
+using std::cin;
+int main()
+{
+    int a;
+    cout<<"Introduzca un numero: ";
+    cin>>a;
+    std::string letranum = std::to_string(a);
+    
+    for (size_t i = 0; i < letranum.length(); i++) {
+        cout << letranum[i];
+        if (i < letranum.length() - 1)
+            cout << "  ";
+    }
+}
