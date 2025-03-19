b0b7fd9b896a7a6708cb5aca29e6904abc1cb5a9
Juan Diego Rubiano <juandirusaenz@gmail.com>
2025-03-19 11:16:12 -0500

    adicion punto c 2.38.cpp

--- Cuarta edición/Capitulo 2/2.38.cpp
+++ Cuarta edición/Capitulo 2/2.38.cpp
@@ -38,3 +38,27 @@ int main(){
     cout<<sum;
 }
 
+//Este programa realiza la serie de taylor e=1+(x^1/1!)+(x2/2!)+(x3/3!)+...+(x^y/n!).
+#include<iostream>
+#include <math.h>
+using std::cout;
+using std::cin;
+using std::endl;
+int main(){
+    int n,m,x;
+    double sum;
+    long long res;
+    cout<<"escriba el valor de x: ";
+    cin>>x;
+    cout<<"escriba un numero mayor a 0 que limite la formula: ";
+    cin>>n;
+    res=1;
+    sum=1;
+    m = 1;
+    while(m<=n){
+        res *= m;
+        sum += (double)pow(x,m)/res;
+        m++;
+    }
+    cout<<sum;
+}
