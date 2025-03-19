269de83b81324a9995374d7f27fe90955f3ef894
Juan Diego Rubiano <juandirusaenz@gmail.com>
2025-03-19 11:04:18 -0500

    punto b 2.38.cpp

--- Cuarta edición/Capitulo 2/2.38.cpp
+++ Cuarta edición/Capitulo 2/2.38.cpp
@@ -16,3 +16,25 @@ int main(){
     }
     cout<<res;
 }
+//Este programa calcula la formaula e=1+(1/1!)+(1/2!)+(1/3!)+...+(1/n!)
+#include<iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+int main(){
+    int n,m;
+    double sum;
+    long long res;
+    cout<<"escriba un numero mayor a 0 que limite la formula: ";
+    cin>>n;
+    res=1;
+    sum=1;
+    m = 1;
+    while(m<=n){
+        res *= m;
+        sum += (double)1/res;
+        m++;
+    }
+    cout<<sum;
+}
+
