4ffbcb62708f566e39c2ffa8d6bd534ff4256650
Juan Diego Rubiano <juandirusaenz@gmail.com>
2025-03-21 09:31:13 -0500

    Create codificacion.cpp

---
+++ Cuarta edición/Capitulo 1/codificacion.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+    int numero contador, digitos, temporal,i;
+    cout<<"ingrese un numero de 4 digitos: ";
+    cin>>numero;
+    
+    temporal = numero;
+    contador = 0;
+    
+    //contamos la cantidad de numeros y se almacena en contador.
+    while(temporal > 0){
+        temp /= 10; //para sacar el ultimo digito
+        contador ++;
+    }
+    
+    int digitos[contador];  //se declara un array llamado digitos de tamaño contador
+    temporal = numero;//Volver a copiar el numero original en temporal
+    
+    for(i)
+}
